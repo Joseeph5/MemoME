5ff1f7533c53b6709c972aa17a84dfb23e2820ea
Youssef Benamor <youssef.beenamor@gmail.com>
2024-11-19 11:47:41 +0100

    update Header

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,16 @@
+import BackgroundPattern from '@/components/BackgroundPattern';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col gap-y-5 justify-center items-center min-h-screen'>
-      <Header />
-
-      {children}
-
-      <Footer />
-    </div>
+    <>
+      <BackgroundPattern />
+      <div className='flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen'>
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 }
