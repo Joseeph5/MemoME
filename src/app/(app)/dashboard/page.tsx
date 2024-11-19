4cffce3835e31813e83c764be8ba790109c65f3d
Youssef Benamor <youssef.beenamor@gmail.com>
2024-11-19 12:15:35 +0100

    update Dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import Branding from '@/components/Branding';
+import Stats from '@/components/Stats';
 import React from 'react';
 
 export default function Page() {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <div className='flex items-center justify-between text-white py-8'>
+        <Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Branding.tsx
@@ -0,0 +1,10 @@
+import H1 from './H1';
+
+export default function Branding() {
+  return (
+    <section>
+      <H1>MemoMe</H1>
+      <p className='text-lg opacity-80'>Manage your code snippets with ease</p>
+    </section>
+  );
+}
